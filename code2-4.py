40ce6c1dbf371dd9873c7dfb0825065c88d6017d
kar7565 <162522040+kar7565@users.noreply.github.com>
2024-03-09 20:18:35 +0900

    Add files via upload

---
+++ code2-4.py
@@ -0,0 +1,19 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 24 2018, 00:16:47) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = int(input("첫 번째 숫자를 입력하세요 : "))
+첫 번째 숫자를 입력하세요 : 3
+>>> b = int(input("두 번째 숫자를 입력하세요 : "))
+두 번째 숫자를 입력하세요 : 1
+>>> result = a + b
+>>> print(a, "+", b, "=", result)
+3 + 1 = 4
+>>> result = a - b
+>>> print(a, "-", b, "=", result)
+3 - 1 = 2
+>>> result = a * b
+>>> print(a, "*", b, "=", result)
+3 * 1 = 3
+>>> result = a / b
+>>> print(a, "/", b, "=", result)
+3 / 1 = 3.0
+>>> 
